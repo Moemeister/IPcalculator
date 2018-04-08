e4ee4d722c36a85dacff059b9fe5db3f6c97ea3f
Moemeister <00062716@uca.edu.sv>
2018-04-07 21:41:00 -0600

    Added functionality

--- app/src/main/java/com/moesystems/ip/MainActivity.java
+++ app/src/main/java/com/moesystems/ip/MainActivity.java
@@ -6,52 +6,53 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    EditText ip, mask,idNet, broad, hosts, netBits, hostBits;
+    EditText ip, netmask,idRed, broadcast, host, netBits, hostBits;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ip = findViewById(R.id.caja_ip);
-        mask = findViewById(R.id.mascara);
-
-        idNet = findViewById(R.id.txt_id_net);
-        broad = findViewById(R.id.txt_broad);
-        hosts = findViewById(R.id.txt_cant_ip);
-        netBits = findViewById(R.id.txt_part_red);
+        netmask = findViewById(R.id.mascara);
+        idRed = findViewById(R.id.txt_id_net);
+        broadcast = findViewById(R.id.txt_broad);
+        host = findViewById(R.id.txt_cant_ip);
         hostBits = findViewById(R.id.txt_part_host);
+        netBits = findViewById(R.id.txt_part_red);
+
     }
-    /*public void calculate(View v) {
-        String[] st = ip.getText().toString().split("\\.");
+    public void calc(View v) {
+        //separa la ip por puntos
+        String[] octeto = ip.getText().toString().split("\\.");
 
-        long rIP = 0;
-        long rMask = 0;
-        long wildcard = 0;
-        int maskInt = Integer.parseInt(mask.getText().toString());
+        int IP = 0;
+        int rMask = 0;
+        int token = 0;
+        int mascara = Integer.parseInt(netmask.getText().toString());
 
-        if(st.length != 4) return;
+        if(octeto.length != 4) return;
 
-        //Get binary ip address
+        //ciclo para obtener la ip en binario
         for(int i=3; i>=0; i--) {
-            rIP |= (Long.parseLong(st[3-i])) << (i*8);
+            IP |= (Long.parseLong(octeto[3-i])) << (i*8);
         }
 
-        //Get binary network mask
-        for(int i=1; i <= maskInt; i++) {
+        //ciclo para obtener la mascara en binario
+        for(int i=1; i <= mascara; i++) {
             rMask |= 1L << (32-i);
         }
 
         //invert network mask
-        wildcard = ~rMask;
+        token = ~rMask;
 
-        idNet.setText(longToIP(rIP & rMask));
-        broad.setText(longToIP(rIP | wildcard));
-        hosts.setText((int)(Math.pow(2, (double)(32-maskInt))) -2 + "");
-        netBits.setText(maskInt + "");
-        hostBits.setText((32-maskInt) + "");
+        idRed.setText(longToIP(IP & rMask));
+        broadcast.setText(longToIP(IP | token));
+        host.setText((int)(Math.pow(2, (double)(32-mascara))) -2 + "");
+        hostBits.setText((32-mascara) + "");
+        netBits.setText(mascara + "");
 
     }
-
+    //binary to IP
     public String longToIP(long ip){
         String st="";
 
@@ -62,15 +63,4 @@ public class MainActivity extends AppCompatActivity {
         return st;
     }
 
-    public void reset(View v) {
-
-        ip.setText("");
-        mask.setText("");
-
-        idNet.setText("");
-        broad.setText("");
-        hosts.setText("");
-        netBits.setText("");
-        hostBits.setText("");
-    }*/
 }
